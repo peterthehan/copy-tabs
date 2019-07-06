9281eb7ebccc5710b9436c16f712061ba5bf8db2
Peter Han <peter.han.9493@gmail.com>
2019-07-06 00:47:55 -0400

    Better UX with copy to clipboard and chrome notification

--- background.js
+++ background.js
@@ -1,6 +1,28 @@
+// https://hackernoon.com/copying-text-to-clipboard-with-javascript-df4d4988697f
+const copyToClipboard = str => {
+  const el = document.createElement('textarea');
+  el.value = str;
+  el.setAttribute('readonly', '');
+  el.style.position = 'absolute';
+  el.style.left = '-9999px';
+  document.body.appendChild(el);
+  el.select();
+  document.execCommand('copy');
+  document.body.removeChild(el);
+};
+
 chrome.browserAction.onClicked.addListener(_ => {
   chrome.tabs.query({ currentWindow: true }, tabs => {
     if (!tabs.length) return;
-    prompt('', tabs.map(tab => tab.url).join('\n'));
+
+    const tabsString = tabs.map(tab => tab.url).join('\n');
+    copyToClipboard(tabsString);
+
+    chrome.notifications.create({
+      type: 'basic',
+      iconUrl: 'assets/clipboard_48.png',
+      title: 'Copied tab URLs!',
+      message: tabsString
+    });
   });
 });
